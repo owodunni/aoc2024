759878c8a9a854ce0a6411afcbefd40cae546fdc
Alexander Poole <alexanderp@opera.com>
2024-12-01 08:41:10 +0100

    Day 1 second problem

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,22 +8,28 @@ fn main() {
     println!("Input file: {file_path}");
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let result = calculate_location_list_distance(contents);
-    println!("Distance: {result}");
+    let distance = calculate_location_list_distance(&contents);
+    println!("Distance: {distance}");
+
+    let similarity = calculate_similarity_index(&contents);
+    println!("Similarity: {similarity}");
 }
 
-fn calculate_location_list_distance(contents: String) -> i64 {
+fn create_list(contents: &String) -> (Vec<i64>, Vec<i64>) {
     let lines = contents.lines();
 
-    let (mut left, mut right): (Vec<i64>, Vec<i64>) = lines
+    lines
         .map(|line| {
             line.split_whitespace()
                 .map(|item| item.parse().unwrap())
                 .collect::<Vec<i64>>()
         })
         .map(|locations| (locations[0], locations[1]))
-        .unzip();
+        .unzip()
+}
 
+fn calculate_location_list_distance(contents: &String) -> i64 {
+    let (mut left, mut right) = create_list(contents);
     left.sort();
     right.sort();
 
@@ -37,20 +43,47 @@ fn calculate_location_list_distance(contents: String) -> i64 {
     distance
 }
 
+fn calculate_similarity_index(contents: &String) -> i64 {
+    let (left, right) = create_list(contents);
+
+    let mut similarity: i64 = 0;
+
+    for left_it in left.iter() {
+        let mut found = 0;
+        for rht_it in right.iter() {
+            if left_it == rht_it {
+                found = found + 1
+            }
+        }
+
+        similarity = similarity + found * left_it;
+    }
+
+    similarity
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        const LOCATION_LIST: &'static str = "3   4
+    const LOCATION_LIST: &'static str = "3   4
 4   3
 2   5
 1   3
 3   9
 3   3";
 
-        let result = calculate_location_list_distance(String::from(LOCATION_LIST));
+    #[test]
+    fn test_location_list_distance() {
+        let location_list = String::from(LOCATION_LIST);
+        let result = calculate_location_list_distance(&location_list);
         assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn test_similarity_index() {
+        let location_list = String::from(LOCATION_LIST);
+        let result = calculate_similarity_index(&location_list);
+        assert_eq!(result, 31);
     }
 }
